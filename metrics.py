ddd70cf1670f8b808b883803e2aaf24a3118a130
Duzzuti <duzzuti@gmail.com>
2024-11-27 22:29:47 +0100

    some new additions with work to do

--- metrics.py
+++ metrics.py
@@ -19,3 +19,19 @@ class SP500Momentum(Metric):
     def normalize(self):
         # Normalize the data
         self.result = utils.linear_weighted_backoff(self.processed, 100, window=1000, min_backoff=0.8, max_backoff=0.85)
+
+class VIX(Metric):
+    def __init__(self):
+        super().__init__()
+
+    def fetch(self):
+        # Fetch the VIX data
+        self.data = utils.fetch_yf_data("^VIX", self.data_dir, self.start_date, self.end_date)["Close"]
+
+    def calculate(self):
+        # no calculation needed
+        self.processed = self.data
+    
+    def normalize(self):
+        # Normalize the data
+        self.result = utils.linear_weighted_backoff(self.processed, 0, window=1000, min_backoff=0.7, max_backoff=0.95, reverse_max=100)
